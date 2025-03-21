43c965432d24922c8c4e5e53a41fca8d376a7a3e
Kakhorosho <159240210+Kakhorosho@users.noreply.github.com>
2025-03-21 23:38:11 +0700

    Update converter.py

--- converter.py
+++ converter.py
@@ -10,7 +10,6 @@ try:
     with open('dataset', 'r', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
-            row = [word(i) for i in row]
             data.append(row)
         print(data)
 except FileNotFoundError:
